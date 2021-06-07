c73040ec9fdeab14f9020a8a3978fd1710126f2b
Jacob Roe <jroenavy24@gmail.com>
2021-06-07 13:40:51 -0700

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class FirstJavaProgram{
+
+    public static void main(String[] agrs) {
+        String myName = "My name is Jacob Roe";
+        String myAge = "I am 26 years old";
+        String myHometown = "My hometown is Santa, Monica CA";
+        System.out.println (myName);
+        System.out.println (myAge);
+        System.out.println (myHometown);
+    }
+}
